9b0329e2c7edbc36c63d244c774405e7b6cb9a14
yarde <planteraminehut@gmail.com>
2023-09-08 21:10:30 +0300

    Initial commit

---
+++ code.js
@@ -0,0 +1,49 @@
+let leaderboardDiv = document.getElementById("leaderboardTable");
+
+// Create a new XMLHttpRequest object
+let xhr = new XMLHttpRequest();
+
+// Configure it to fetch the JSON file
+xhr.open('GET', 'data.json', true);
+
+
+
+// Set up a callback function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 400) {
+        // If the request was successful, parse the JSON
+        const data = JSON.parse(xhr.responseText);
+        for (const i of data.keys() ) {
+            const game = data[i]
+            let gameRow = document.createElement("tr");
+            let posCol = document.createElement("td")
+            posCol.textContent = `#${i + 1}: `;
+            gameRow.appendChild(posCol)
+            let nameCol = document.createElement("td")
+            nameCol.textContent = game.username;
+            gameRow.appendChild(nameCol)
+            let timeCol = document.createElement("td");
+            timeCol.textContent = game.time.toString().substring(0, 6);
+            gameRow.appendChild(timeCol);
+            let replayCol = document.createElement("td");
+            replayCol.innerHTML = `<a href = ${game.url}> Link</a>`;
+            gameRow.appendChild(replayCol);
+            let platformCol = document.createElement("td");
+            platformCol.innerHTML = `<image src = ${game.platform}.ico height=16 width=16 \\>`;
+            gameRow.appendChild(platformCol);
+            leaderboardDiv.appendChild(gameRow);
+        }
+        console.log(data);
+    } else {
+        // If there was an error, handle it here
+        console.error('Error loading JSON file:', xhr.status, xhr.statusText);
+    }
+};
+
+// Handle network errors
+xhr.onerror = function() {
+    console.error('Network error while trying to fetch JSON file');
+};
+
+// Send the request
+xhr.send();
---
+++ getter.js
@@ -0,0 +1,71 @@
+main().then(() => {
+
+
+});
+
+
+async function main() {
+    const tetrioData = await getTetrioLeaderboards();
+
+    const tetrioStandard = tetrioData.data.records.map(standardizeTetrio);
+    const jstrisData = await getJstrisLeaderboards();
+    const jstrisStandard = jstrisData.map(standardizeJstris);
+
+    const jstrisUnique = jstrisStandard.filter((game) => {
+        const matchingTetrio = tetrioStandard.find((tetrioGame) => tetrioGame.username.toLowerCase() == game.username.toLowerCase())
+        if (matchingTetrio != undefined && matchingTetrio.time > game.time) {
+            return false;
+        }
+        return true;
+    })
+
+    const tetrioUnique = tetrioStandard.filter((game) => {
+        const matchingJstris = jstrisStandard.find((jstrisGame) => jstrisGame.username.toLowerCase() == game.username.toLowerCase())
+        if (matchingJstris != undefined && matchingJstris.time > game.time) {
+            return false;
+        }
+        return true;
+    })
+
+
+    console.log(JSON.stringify(tetrioUnique.concat(jstrisUnique).sort((a,b) => a.time - b.time)));
+
+}
+
+function standardizeTetrio(record) {
+    let res = {};
+    res.time = record.endcontext.finalTime / 1000;
+    // res.pieces = record.endcontext.piecesplaced;
+    // res.pps = res.pieces / res.time;
+    // res.keys = record.endcontext.inputs;
+    // res.kpp = res.keys / res.pieces;
+    // ^^ commented out because jstris dosen't support this ^^
+    res.username = record.user.username;
+    res.url = "https://tetr.io/#r:" + record.replayid;
+    res.platform = "tetrio"
+    return res;
+}
+
+function standardizeJstris(record) {
+    let res = {};
+    res.time = record.game;
+    res.username = record.name;
+    res.url = "https://jstris.jezevec10.com/replay/" + record.id;
+    res.platform = "jstris"
+    return res;
+}
+
+
+async function getTetrioLeaderboards() {
+    let url = "https://ch.tetr.io/api/streams/40l_global";
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+}
+
+async function getJstrisLeaderboards() {
+    let url = "https://jstris.jezevec10.com/api/leaderboard/1?mode=1&offset=0";
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+}
